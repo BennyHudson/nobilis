5a2b4f0662e7495a820d78da2e1dcccc950958cc
Ben Hudson <ben.hudson@vodafone.com>
2024-10-01 21:01:34 +0100

    Removing wflogs

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1728415127;s:7:"authKey";s:64:"Yrmz:@|)P@UJEV9Cl^$$5EON]~.m`45e()pkJ`FUgt~=q]^J1CohdN+o]8Oqe|P:";s:7:"version";s:5:"1.1.0";}
