9ac1376a296dc126b2f3acb58c13015861e30c76
Ben Hudson <ben.hudson@vodafone.com>
2024-10-25 13:57:55 +0100

    Updating contact form to be dynamic

--- wp-content/themes/nobilis/partials/quick-contact.php
+++ wp-content/themes/nobilis/partials/quick-contact.php
@@ -4,10 +4,8 @@
     <div class="overlay-content">
       <div class="overlay-form">
         <a href="#" class="overlay-close"><i class="fa fa-times-circle fa-2x"></i></a>
-        <h2>Make an enquiry</h2>
-		  <p>For any general enquiries, please feel free to contact our team on 0800 862 0650.</p>
-		  <p>If you're looking into care options for yourself or a loved one, or would like more information about our care packages, our dedicated Sales team is here to help on 0808 175 3980.</p>
-		  <p>We're here to support you every step of the way.</p>
+        <h2><?php the_field('contact_form_title', 2); ?></h2>
+        <?php the_field('contact_form_content', 2); ?>
         <?php echo do_shortcode('[contact-form-7 id="243" title="Main Contact Form"]'); ?>
       </div>
     </div>
